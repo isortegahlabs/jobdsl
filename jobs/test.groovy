2bd5ea397870fa4afed7b585f4e1a3af0de630e3
ivansebastian.ortega <ivansebastian.ortega@bbva.com>
2020-02-16 20:42:22 -0600

    First commit

---
+++ jobs/test.groovy
@@ -0,0 +1,23 @@
+project_name = "jenkins-pipeline-python"
+repo = "https://github.com/videocursoscloud/test-jenkins-1.git"
+repo_name = "repo"
+
+pipelineJob(project_name) {
+    definition {
+        triggers {
+            scm('H/1 * * * *')
+        }
+
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        name(repo_name)
+                        url(repo)
+                    }
+                }
+                scriptPath("Jenkinsfile")
+            }
+        }
+    }
+}
