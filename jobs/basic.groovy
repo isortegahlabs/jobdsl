4f8355511bfc0124a669fccf5097e5f2bc12a6f8
ivansebastian.ortega <ivansebastian.ortega@bbva.com>
2020-02-17 22:35:47 -0600

    folder

--- jobs/basic.groovy
+++ jobs/basic.groovy
@@ -1,14 +1,16 @@
 def project = 'isortegahlabs/jenkinsfiles_project'
 def branchApi = new URL("https://api.github.com/repos/${project}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+folder("project"){
+    description('Folder for project A')
+}
+
 branches.each {
     def branchName = it.name
-    def jobName = "${project}-${branchName}".replaceAll('/','-')
-    folder("project"){
-        job(jobName) {
-            scm {
-                git("git://github.com/${project}.git", branchName)
-            }
+    def jobName = "project/"+"${project}-${branchName}".replaceAll('/','-')
+    job(jobName) {
+        scm {
+            git("git://github.com/${project}.git", branchName)
         }
     }
 }
