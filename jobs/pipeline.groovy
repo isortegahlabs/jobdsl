e004231987e3b88aefb831067b4eb835bbb8838d
ivansebastian.ortega <ivansebastian.ortega@bbva.com>
2020-03-15 21:03:27 -0600

    Test cpsScm

---
+++ jobs/pipeline.groovy
@@ -0,0 +1,44 @@
+def project = 'isortegahlabs/jenkinsfiles_project'
+def branchApi = new URL("https://api.github.com/repos/${project}/branches")
+def branchesRepo = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+
+def folderName = "pipeLineSimple"
+folder(folderName){
+  description('Folder for project A')
+}
+
+print branchesRepo
+branchesRepo.each{
+	def branchName = it.name
+	def jobName = "${folderName}/"+"${project}-${branchName}".replaceAll('/','-')
+    pipelineJob(jobName) {
+
+    //triggers {
+      //scm('H/5 * * * *')
+    //}
+
+    //triggers {
+      //cron('H/5 * * * *')
+    //}
+
+    description("Pipeline for $jobName")
+
+    definition {
+      cpsScm {
+        scm {
+          git {
+            remote { url('https://github.com/isortegahlabs/jenkinsfiles_project') }
+            branches(branchName)
+            scriptPath('Jenkinsfile')
+            extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+          }
+
+          // the single line below also works, but it
+          // only covers the 'master' branch and may not give you
+          // enough control.
+          // git(repo, 'master', { node -> node / 'extensions' << '' } )
+        }
+      }
+    }
+    }
+}
---
+++ jobs/pipelineMultiBranch.groovy
@@ -0,0 +1,62 @@
+def project = 'isortegahlabs/jenkinsfiles_project'
+
+def folderName = "SharedLibraries"
+folder(folderName){
+  description('Folder for project A')
+}
+multibranchPipelineJob("${folderName}/baseProject"){
+  
+  displayName("SharedLibraries Project")
+  description("Proyecto de prueba SharedLibraries")
+  
+  factory {
+    workflowBranchProjectFactory {
+      scriptPath('Jenkinsfile')
+    }
+  }
+
+  orphanedItemStrategy {
+    discardOldItems {
+      numToKeep(5)
+      daysToKeep(2)
+    }
+  }
+
+  branchSources {
+    git {
+      id('shared') 
+      remote('https://github.com/isortegahlabs/jenkinsfiles_project')
+      //credentialsId('github-ci')
+      includes('feature/sha*')
+    }
+    /*git {
+      id('shareds') 
+      remote('https://github.com/isortegahlabs/jenkinsfiles_project')
+      //credentialsId('github-ci')
+      includes('feature/basi*')
+    }*/
+    /*
+    branchSource {
+        source {
+          bitbucket {
+            credentialsId("top-secret-1234-some-guid")
+            repoOwner("${bitbucket_project.toUpperCase()}")
+            repository("${bitbucket_repo}")
+            serverUrl("https://bitbucket.acme.com/")
+            traits {
+              headWildcardFilter {
+                includes("master release/* feature/* bugfix/*")
+                excludes("")
+              }
+            }
+          }
+        }
+    */
+  }
+
+  triggers {
+    cron('H/5 * * * *')
+  }
+
+}
+
