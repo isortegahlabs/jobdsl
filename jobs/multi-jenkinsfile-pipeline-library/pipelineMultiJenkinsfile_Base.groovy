ac7aba7bfd4683b81208a71c042aeaa995fbf80a
isortegahlabs <ivansebastian.ortega@bbva.com>
2022-04-25 15:38:04 -0500

    [UPDATE] pipelineMultiJenkinsfile

--- jobs/multi-jenkinsfile-pipeline-library/pipelineMultiJenkinsfile_Base.groovy
+++ jobs/multi-jenkinsfile-pipeline-library/pipelineMultiJenkinsfile_Base.groovy
@@ -1,18 +1,10 @@
-def project = 'isortegahlabs/jenkinsfiles_project'
+def project = 'isortegahlabs/multi-Jenkinsfile-pipeline-library'
 def branchApi = new URL("https://api.github.com/repos/${project}/branches")
 def branchesRepo = new groovy.json.JsonSlurper().parse(branchApi.newReader())
 
-def folderName = "pipelineMultiJenkinsfile"
+def folderName = "pipelineMultiJenkinsfiles"
 folder(folderName){
-  description('Folder for project Pipeline simple')
-}
-
-branchesRepo.removeAll{
-  it.name.contains("shared")
-}
-
-branchesRepo.removeAll{
-  it.name.contains("master") 
+  description('Folder for project Pipeline Multi Jenkinsfile')
 }
 
 branchesRepo.each{
@@ -35,16 +27,11 @@ branchesRepo.each{
       cpsScm {
         scm {
           git {
-            remote { url('https://github.com/isortegahlabs/jenkinsfiles_project') }
+            remote { url('https://github.com/isortegahlabs/multi-Jenkinsfile-pipeline-library') }
             branches(branchName)
-            scriptPath('Jenkinsfile')
+            scriptPath('base.Jenkinsfile')
             extensions { }  // required as otherwise it may try to tag the repo, which you may not want
           }
-
-          // the single line below also works, but it
-          // only covers the 'master' branch and may not give you
-          // enough control.
-          // git(repo, 'master', { node -> node / 'extensions' << '' } )
         }
       }
     }
---
+++ jobs/multi-jenkinsfile-pipeline-library/pipelineMultiJenkinsfile_Second.groovy
@@ -0,0 +1,39 @@
+def project = 'isortegahlabs/multi-Jenkinsfile-pipeline-library'
+def branchApi = new URL("https://api.github.com/repos/${project}/branches")
+def branchesRepo = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+
+def folderName = "pipelineMultiJenkinsfiles"
+folder(folderName){
+  description('Folder for project Pipeline Multi Jenkinsfile')
+}
+
+branchesRepo.each{
+	def branchName = it.name
+
+	def jobName = "${folderName}/"+"${project}-second".replaceAll('/','-')
+    pipelineJob(jobName) {
+
+    //triggers {
+      //scm('H/5 * * * *')
+    //}
+
+    //triggers {
+      //cron('H/5 * * * *')
+    //}
+
+    description("Pipeline for $jobName")
+
+    definition {
+      cpsScm {
+        scm {
+          git {
+            remote { url('https://github.com/isortegahlabs/multi-Jenkinsfile-pipeline-library') }
+            branches(branchName)
+            scriptPath('second.Jenkinsfile')
+            extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+          }
+        }
+      }
+    }
+    }
+}
